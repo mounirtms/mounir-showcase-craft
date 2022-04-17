f63d5b0b38840d52d2d0da9000d643461896a0ac
mabderrahmani <mabderrahmani@hotech.systems>
2022-04-17 17:05:21 +0100

    few updates and tunings ..

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 require('../css/index.css');
 require('../css/style.css');
+require('../css/about.css');
+require('../css/resume.css');
+require('../css/work.css');
 require('../css/animate.css'); 
 
 window.$ = require('jquery');
