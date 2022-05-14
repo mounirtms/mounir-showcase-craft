49a75dd0152bb5bacec1f140821ac8f38ffedb67
mabderrahmani <mabderrahmani@hotech.systems>
2022-05-14 12:01:21 +0100

    few work updates

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ require('../css/resume.css');
 require('../css/work.css');
 require('../css/animate.css');
 
-require('./twcore.js');
+
 window.$ = require('./jQuery.js');
 require('./particles.js');
 require('./index.js');
